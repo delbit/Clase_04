e8a347f6b152f753da3b5047864b60b33bd228cf
David Lucena <delbits@gmail.com>
2021-04-05 16:14:31 -0300

    Refactory de Funciones

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
  * Script
  * @author: David Eloy Lucena Rey
  * @date: 30/03/2021
+ * @resume: El siguiente script es un simulador de cuotas en el método francés, donde todas las cuotas son iguales.
+ * para este desafió se agregaron 3 nuevas funciones, se realizaron funciones especificas en para complementar la función calculoCuota
+ * y se llevo todo a una función el código necesario a na función llamada main.
+ * por lo tanto main se encarga de llamar a las demás funciones.
  * */
 
 // Funciones
@@ -45,19 +49,34 @@ function ingresoInteres() {
     
 }
 
-function calculoCuota(capital, meses, interes) {
-  let interesMensual = interes / 1200;
-  let factor = Math.pow(interesMensual + 1,meses);
-  let cuota = capital * interesMensual * factor / (factor - 1);
-  return cuota;
+function factorFunc(interesMensual, meses) {
+  return (Math.pow(interesMensual + 1,meses));
 }
 
-//mostramos el resultado
+function cuotaFunc(capital, interesMensual, factor) {
+  return (capital * interesMensual * factor / (factor - 1));
+}
 
-let capital = ingresoCapital();
-let meses = ingresoMeses();
-let interes = ingresoInteres();
+function calculoCuota(capital, meses, interes) {
+  let interesMensual = interes / 1200;
+  let factor = factorFunc(interesMensual,meses);
+  return cuotaFunc(capital, interesMensual, factor);
+}
 
-let cuota = calculoCuota(capital, meses, interes);
+// Funcion principal
 
-alert ("La cuota de " + capital + " a " + meses + " meses y interés anual de " + interes +  "% es de " + (cuota.toFixed(2)));
+function  main(){
+  let capital = ingresoCapital();
+  let meses = ingresoMeses();
+  let interes = ingresoInteres();
+
+  let cuota = calculoCuota(capital, meses, interes);
+
+  alert ("La cuota de " + capital + " a " + meses + " meses y interés anual de " + interes +  "% es de " + (cuota.toFixed(2)));
+}
+
+// Inicio del programa
+
+main();
+
+// Fin programa
